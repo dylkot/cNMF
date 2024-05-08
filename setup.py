e746b7cfea8f6fec20710210053ec901b0ca933c
Dylan Kotliar <dylkot@gmail.com>
2024-05-07 22:07:09 -0400

    added __version__ attribute

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+# Function to read the version from version.py
+def get_version():
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(base_dir, 'src/cnmf/version.py')) as f:
+        locals = {}
+        exec(f.read(), locals)
+        return locals['__version__']
+
 setuptools.setup(
     name="cnmf",
-    version="1.5.0",
+    version=get_version(),
     author="Dylan Kotliar",
     author_email="dylkot@gmail.com",
     description="Consensus NMF for scRNA-Seq data",
--- src/cnmf/__init__.py
+++ src/cnmf/__init__.py
@@ -1,2 +1,4 @@
 from .cnmf import main, cNMF, save_df_to_npz, load_df_from_npz
 from .preprocess import Preprocess
+
+from .version import __version__
